40ddf6750b4a5c492b856a9dac6c35a5ad9c5d33
Anurag Pal <apal895@gmail.com>
2023-02-24 00:18:08 +0530

    chirp model changed

--- src/models/user/tph/chirps.ts
+++ src/models/user/tph/chirps.ts
@@ -2,34 +2,35 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const TphChirpsSchema = new Schema(
-    {
-        event: {
-            type: String,
-            required: true,
-            ref: "FrattyEvents",
-        },
-        wallet: {
-            type: String,
-            required: true,
-            index: true,
-            default: "false",
-        },
-        text: {
-            type: String,
-            default: "false",
-        },
-        image: {
-            type: String,
-            default: "false",
-        },
+  {
+    event: {
+      type: String,
+      required: true,
+      ref: "FrattyEvents",
     },
-    {
-        timestamps: true,
-    }
+    wallet: {
+      type: String,
+      required: true,
+      index: true,
+      default: "false",
+    },
+    text: {
+      type: String,
+      default: "false",
+    },
+    image: {
+      type: String,
+      default: "false",
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
-
-
-
 
 const TphChirps = mongoose.model("TphChirps", TphChirpsSchema);
 export default TphChirps;
